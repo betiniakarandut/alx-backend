5b9bf22ab61d174321462fc19f738123513486ff
betiniakarandut <betiniakarandut@gmail.com>
2023-08-21 23:48:01 +0000

    Pagination: return a specific page

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""Module for simple_helper_function.py"""
+import csv
+import math
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """Return a tuple of size two
+
+    Args:
+        page(int) - page number
+        page_size(int) - page size
+
+    Returns:
+        A tuple
+    """
+    end_index: int = 0
+    for page_num in range(page):
+        start_index: int = page_num * page_size
+        end_index += page_size
+    return(start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returns a page within the specified page size"""
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+
+        start_idx, end_idx = index_range(page, page_size)
+        data = self.dataset()
+        if start_idx > len(data):
+            return[]
+
+        return data[start_idx:end_idx]
+
+
+# test
+server = Server()
+
+try:
+    should_err = server.get_page(-10, 2)
+except AssertionError:
+    print("AssertionError raised with negative values")
+
+try:
+    should_err = server.get_page(0, 0)
+except AssertionError:
+    print("AssertionError raised with 0")
+
+try:
+    should_err = server.get_page(2, 'Bob')
+except AssertionError:
+    print("AssertionError raised when page and/or page_size are not ints")
+
+
+print(server.get_page(1, 3))
+print(server.get_page(3, 2))
+print(server.get_page(3000, 100))
