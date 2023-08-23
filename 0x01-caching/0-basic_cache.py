0039c322acdf8d4c5176c6534d684a0aeba66ab1
betiniakarandut <betiniakarandut@gmail.com>
2023-08-23 10:30:15 +0000

    Work: Started working on tasks

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""BasicCache that inherits from BaseCaching"""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    A caching system.
+    """
+    def __init__(self):
