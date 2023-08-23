8183c692e3146cde262b429431903baae815ea2b
betiniakarandut <betiniakarandut@gmail.com>
2023-08-23 22:22:31 +0000

    Basic: A basic cache system

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,3 +8,17 @@ class BasicCache(BaseCaching):
     A caching system.
     """
     def __init__(self):
+        super().__init__()
+
+    def put(self, key, item):
+        """stores key and value in the cache"""
+        if item is None:
+            return
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves value in the cached data linked to key."""
+        if key is None or not key:
+            return None
+        else:
+            return self.cache_data.get(key)
