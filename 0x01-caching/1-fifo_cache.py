0b1b2a01214381c25574bb7b408691edeb70a572
betiniakarandut <betiniakarandut@gmail.com>
2023-08-24 04:07:01 +0000

    Delete: Blanks || whitespaces)

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -44,16 +44,3 @@ class FIFOCache(BaseCaching):
                 return None
             else:
                 return self.cache_data[key]
-
-my_cache = FIFOCache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.put("D", "School")
-my_cache.print_cache()
-my_cache.put("E", "Battery")
-my_cache.print_cache()
-my_cache.put("C", "Street")
-my_cache.print_cache()
-my_cache.put("F", "Mission")
-my_cache.print_cache() 
