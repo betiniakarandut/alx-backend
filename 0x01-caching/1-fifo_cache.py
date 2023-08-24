9adf54fbd825b6c5f3b072fd7e6c7caa5a67a48c
betiniakarandut <betiniakarandut@gmail.com>
2023-08-24 01:41:16 +0000

    FIFO: Implementation

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,40 @@
+ #!/usr/bin/env python3
+"""LIFOCache module inherits from BaseCaching"""
+from collections import deque
+
+BaseCaching = __import__("base_caching").BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """A caching system."""
+    def __init__(self):
+        super().__init__()
+        self.queue = deque()
+
+    def put(self, key, item):
+        """stores key and value in the cache
+
+        Args:
+            key[str] - to check
+            item - value of key
+
+        Return:
+            None if no item else
+            implement a FIFO caching
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            discard = self.queue.popleft()
+            print(f"DISCARD: {discard}")
+            del self.cache_data[discard]
+
+        self.queue.append(key)
+        self.cache_data[key] = item
+
+        def get(self, key):
+            if key is None or not self.cache_data:
+                return None
+            else:
+                return self.cache_data.get(key)
