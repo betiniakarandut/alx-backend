14d43ea06310914fbd2c54e1d30a785b067c1f2e
betiniakarandut <betiniakarandut@gmail.com>
2023-08-29 22:11:33 +0100

    Type annotated 0-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python3
+"""Module for 0-app.py"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Render a basic flask app
 @app.route('/')
-def index_0():
+def index_0() -> str:
     return render_template(f'0-index.html')
 
 if __name__ == '__main__':
